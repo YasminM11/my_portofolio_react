2dfd9e4514f14e8f101bc620e3867bcfed17bd34
Yasmin <yasmin312002@hotmail.com>
2021-03-07 13:10:31 -0800

    create new project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
+import React from "react";
 import './App.css';
+import {BrowserRouter as Router, Switch, Route, Link} from "react-router-dom";
+import Container from 'react-bootstrap';
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      title: "Yasmin Portfolio",
+      headerLink: [
+        {title: 'Home', path: '/'},
+        {title: 'About', path: '/about'},
+        {title: 'Contact', path: '/contact'}
+      ],
+      home: {
+        title: "Yasmin's Portfolio",
+        subTitle: "Landing my first job!",
+        text: "Checkout my projects"
+      },
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+      about: {
+         title: "About",
+      },
+      contact: {
+        title: "Contacts",
+      }
+    }
+  }
+  render () {
+      return (
+      <Router>
+        <Container fluid={true}>
+
+
+
+        </Container>
+
+
+      </Router>
+    );
+
+  }
+  
 }
 
 export default App;
